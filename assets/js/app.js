40abf7837ab3d5e183064d3b7a576dcd24e48fde
ughurba <ughurba@edu.code.az>
2022-05-16 16:47:44 +0400

    fix-bug in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,28 @@
-const bodyTabs = document.querySelectorAll('.body-mask');
-const hamTabs = document.querySelectorAll('.ham-menu');
+
+const hamTabs  = document.querySelectorAll(".ham-menu");
+const hamList  = document.querySelector(".ham-list");
+console.log(hamList)
+hamTabs.forEach(function (item) {
+  item.addEventListener("click", function () {
+    let hamBtn = item;
+    let hamId = hamBtn.getAttribute("data-ham");
+    let hamTab = document.querySelector(hamId);
+
+
+    hamTabs.forEach(function(item){
+        item.classList.remove("active")
+    });
+    
+   
+    
+
+    hamTab.classList.add("active");
+
+  
+  });
+});
+
+
+
+
+
